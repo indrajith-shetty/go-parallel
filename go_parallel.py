dcdcb8454d4852accb9ec9ebc7b3c69fbef93dd2
Indrajith Shetty <indrajitshetty@gmail.com>
2021-05-16 16:07:29 -0700

    Update go_parallel.py

--- go_parallel.py
+++ go_parallel.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 from multiprocessing import Pipe, connection, Process
+from threading import Thread
+from typing import Union
 
 
 @dataclass
@@ -7,7 +9,8 @@ class ParallelRunner:
     _recv_conn: connection.Connection = None
     _send_conn: connection.Connection = None
     _result: object = None
-    method: str = 'multiprocessing'
+    _p: Union[Thread, Process] = None
+    _method: str = 'multiprocessing'
 
     def __post_init__(self):
         self._recv_conn, self._send_conn = Pipe(duplex=False)
@@ -18,22 +21,22 @@ class ParallelRunner:
         conn.close()
 
     def start(self, func, args):
-        self.p = Process(target=self._executor, args=(self._send_conn, func, args))
+        self._p = Process(target=self._executor, args=(self._send_conn, func, args))
         # self.p = Thread(target=self.executor, args=(self.send_conn, func, args))
-        self.p.start()
+        self._p.start()
 
     def _join(self):
-        self.p.join()
-        self._recv_conn.close()
+        self._p.join()
 
-    def get_result(self):
+    def get_result(self) -> object:
         if self._result is None:
             self._join()
             self._result = self._recv_conn.recv()
+            self._recv_conn.close()
         return self._result
 
 
-def gorun(func, *args) -> object:
-    gparallel = ParallelRunner()
-    gparallel.start(func, args)
-    return gparallel
+def gorun(func, *args) -> ParallelRunner:
+    runner = ParallelRunner()
+    runner.start(func, args)
+    return runner
