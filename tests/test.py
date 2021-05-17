a965db71acf8da2547b2223b101d2a672c2a68a9
Indrajith Shetty <indrajitshetty@gmail.com>
2021-05-17 06:19:22 -0700

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -2,21 +2,14 @@ import time
 
 from go_parallel.go_parallel import gorun
 
-a = "sdfsdfsfsdfsdfsds"
-
 
 def f(name):
-    global a
     print(name)
-    print(a)
-    a = "empty f"
     return name + " is appended"
 
 
 def sleeper(seconds):
-    global a
     time.sleep(seconds)
-    # print(a)
     a = "empty sleeper"
 
     return a
@@ -24,6 +17,6 @@ def sleeper(seconds):
 
 if __name__ == "__main__":
     runner = gorun(sleeper, 5)
-    runner2 = gorun(f, "indra")
+    runner2 = gorun(f, "i")
     print(runner2.get_result())
     print(runner.get_result())
